a48dc3b2ff2d318863d26f959bfe76786155e01a
matteowijck-crypto <matteowijck@gmail.com>
2026-09-20 13:08:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cadenceur-v2';
+const CACHE='cadenceur-v3';
 const FILES=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
